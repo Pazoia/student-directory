b4374ba649062905fa0915e630c176797219cfc4
Pazoia <pazoia.gomes@gmail.com>
2020-01-14 19:19:24 +0000

    refactoring '.each' to 'while loop' method

--- directory_copy.rb
+++ directory_copy.rb
@@ -17,17 +17,27 @@ def print_header
 end
 
 def print(students)
+  count = 0
+  while count != students.count do
+    puts "#{students[count][:name]} (#{students[count][:cohort]} cohort)"
+    count += 1
+  end
+end
+
+=begin 
+def print(students)
   puts "Please type the names you want to display, first letter: "
   puts "Type 'all' to print the full list."
   initial = gets.chomp
   students.each_with_index do |student, indx|
     if initial == "all"
       puts "#{indx += 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    elsif student[:name].chars.first == initial
+    elsif student[:name].chars.first == initial && student[:name].length < 12
       puts "#{indx += 1}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
 end
+=end
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
